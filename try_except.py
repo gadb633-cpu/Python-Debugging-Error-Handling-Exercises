88aa8680033c73915debc06d2a20069e35e8807a
gad-biton <gadb633@gmail.com>
2026-06-29 15:55:31 +0300

    added 2. Safe Division

--- try_except.py
+++ try_except.py
@@ -1,8 +1,15 @@
 # 1. Age Converter
-try:    
-    age = input("Enter your age: ")
-    next_year = int(age) + 1
-    print("Next year you will be", next_year)
-except ValueError:
-    print("Age must be a number ")
+# try:    
+#     age = input("Enter your age: ")
+#     next_year = int(age) + 1
+#     print("Next year you will be", next_year)
+# except ValueError:
+#     print("Age must be a number ")
+# 2. Safe Division
+try:
+    a = int(input("First number: "))
+    b = int(input("Second number: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("Cannot divide by zero ")
     
